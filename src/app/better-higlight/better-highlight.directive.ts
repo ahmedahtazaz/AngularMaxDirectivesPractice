5c3f21f40aea013719df25fdcebd5e46748ed2da
Ahtazaz Ahmed Khan <49054500+ahmedahtazaz@users.noreply.github.com>
2020-09-25 16:18:11 +0500

    Dynamic attribute directive binding.

--- src/app/better-higlight/better-highlight.directive.ts
+++ src/app/better-higlight/better-highlight.directive.ts
@@ -1,27 +1,34 @@
-import { Directive, HostBinding, HostListener, OnInit } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Directive, HostBinding, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
-  @HostBinding('style.color') color: string = 'black';
+  @Input('defaultBgColor') defaultBgColor: string = 'transparent';
+  @Input('defaultTextColor') defaultTextColor: string = 'black';
+  @Input('highlightBgColor') highlightBgColor: string = 'green';
+  @Input('highlightTextColor') highlightTextColor: string = 'yellow';
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+  @HostBinding('style.color') color: string;
 
   constructor() { }
   
   ngOnInit(): void {
-    
+    this.backgroundColor = this.defaultBgColor;
+    this.color = this.defaultTextColor;
   }
 
   @HostListener('mouseenter') mouseHover = (event: Event) => {
-    this.backgroundColor = 'green';
-    this.color = 'yellow';
+    this.backgroundColor = this.highlightBgColor;
+    this.color = this.highlightTextColor;
   }
 
   @HostListener('mouseleave') mouseLeave = (event: Event) => {
-    this.backgroundColor = 'transparent';
-    this.color = 'black';
+    this.backgroundColor = this.defaultBgColor;
+    this.color = this.defaultTextColor;
   }
 
 }
