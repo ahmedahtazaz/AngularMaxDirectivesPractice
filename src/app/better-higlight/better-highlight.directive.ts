676a8470c2a0deeb4d8d3593c66a5fd6c2b581a9
Ahtazaz Ahmed Khan <49054500+ahmedahtazaz@users.noreply.github.com>
2020-09-25 15:48:43 +0500

    HostListener added for dynamic styling better-highlight-directive.

--- src/app/better-higlight/better-highlight.directive.ts
+++ src/app/better-higlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -8,8 +8,17 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
   
   ngOnInit(): void {
+    
+  }
+
+  @HostListener('mouseenter') mouseHover = (event: Event) => {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'yellow');
+  }
+
+  @HostListener('mouseleave') mouseLeave = (event: Event) => {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black');
   }
 
 }
